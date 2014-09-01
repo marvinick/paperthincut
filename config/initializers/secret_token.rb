07d46792e8956fd0cf90c8800c8204ff2eb46321
marvinick <marviluw@gmail.com>
2014-09-01 10:11:36 -0500

    gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paperthincut::Application.config.secret_key_base = '9f8463da53068995996a87feb1ae47e0bfca03029a87755a07f238309894c7f21bf3e5a1667cf3646a0a70adf6a4a1e8a873eac38148d6ce817dea5a55ce562b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paperthincut::Application.config.session_store :cookie_store, key: '_paperthincut_session'
