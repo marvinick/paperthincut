434327f509fb43f6bb8b774135473a3bfd1aa9b7
marvinick <marviluw@gmail.com>
2014-09-12 03:03:32 -0500

    post update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -21,9 +23,36 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to members_url }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
   end
+
+  def set_post
+      @post = Post.find(params[:id])
+    end
+
 end
