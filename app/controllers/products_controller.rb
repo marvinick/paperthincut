88c89f03d4fcf58b050446017fd12634ce2cab2e
marvinick <marviluw@gmail.com>
2014-09-13 20:16:21 -0500

    category + navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :image_url, :price)
+    params.require(:product).permit(:name, :description, :image_url, :price, vendor_ids: [])
   end
 end
