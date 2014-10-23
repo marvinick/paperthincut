0552f8fbeaafa124737264a4fa16d4f622a6d352
marvinick <marviluw@gmail.com>
2014-10-23 16:16:54 -0500

    request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,20 @@
 class RequestsController < ApplicationController
+
   def index
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    @create = Request.new(request_params)
+
+    if @request.save
+      redirect_to distributors_path
+    else
+      render :new
+    end
   end
 
   def show
