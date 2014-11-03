94c5e5c0ab254b4f53c812050fcf6f79c52d227b
marvinick <marviluw@gmail.com>
2014-11-02 20:18:22 -0600

    order email

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,6 +1,6 @@
 class AppMailer < ActionMailer::Base
   def request_deliver(request)
     @request = request
-    mail to: @request.email, from: "marvkiluw@gmail.com", subject: "Next delivery day order"
+    mail to: @request.email, from: "marvkiluw@gmail.com", subject: "Order from Marvin - In The Raw Broken Arrow"
   end
 end
