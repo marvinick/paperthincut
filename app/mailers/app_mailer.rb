bc884e6956e57bdc100f10137dd197326915c1f7
marvinick <marviluw@gmail.com>
2014-11-20 20:59:44 -0600

    form request

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,8 +1,6 @@
 class AppMailer < ActionMailer::Base
   def request_deliver(request)
     @request = request
-    if params[:distributor_ids]
-      mail to: @request.email, from: "marvkiluw@gmail.com", subject: "Order from Marvin - In The Raw Broken Arrow"
-    end
+    mail to: @request.email, from: "marvkiluw@gmail.com", subject: "Order from Marvin - In The Raw Broken Arrow"
   end
 end
