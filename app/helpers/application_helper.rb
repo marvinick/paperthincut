097a4f621e2d4ceed17f0065e0f2dca5a67c82af
marvinick <marviluw@gmail.com>
2014-11-27 07:04:00 -0600

    timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(url)
+    url.starts_with?("http") ? url : "http://#{url}"
+  end
+
+  def nice_date(dt)
+    dt.strftime("%m/%d/%Y 1:%M%P %Z") # 03/14/2013 9:09pm UTC
+  end
 end
