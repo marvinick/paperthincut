087bbd73eaea4419a13f0210e4ea6598dad2e80d
marvinick <marviluw@gmail.com>
2014-11-30 22:59:34 -0600

    inventories create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :send_inventory
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -18,5 +18,10 @@ class ApplicationController < ActionController::Base
       flash[:error] = "You need to be logged in to do that."
       redirect_to root_path
     end
+  end
+
+  def send_inventory(inventory)
+    @inventories = inventory
+    mail to: @inventory.email, from: "marvin8214@gmail.com", subject: "Last Month Sushi Bar Inventory - ITRBA"
   end
 end
--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,11 +1,11 @@
 class AppMailer < ActionMailer::Base
   def request_deliver(request)
     @request = request
-    mail to: @request.email, from: "marv@gmail.com", subject: "Order from Marvin - In The Raw Broken Arrow"
+    mail to: @request.email, from: "marvkiluw@gmail.com", subject: "Order from Marvin - In The Raw Broken Arrow"
   end
 
   def send_inventory(inventory)
     @inventory = inventory
-    mail to: @inventory.email, from: "marvin8214@gmail.com", subject: "Last Month Sushi Bar Inventory - ITRBA"
+    mail to: inventory.email, from: "marvin8214@gmail.com", subject: "Last Month Sushi Bar Inventory - ITRBA"
   end
 end
