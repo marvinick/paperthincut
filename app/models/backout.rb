f6b8b1d4b9a9d9bee97e5581639688939280ccbb
marvinick <marviluw@gmail.com>
2014-12-10 23:23:26 -0600

    paperthin

--- app/models/backout.rb
+++ app/models/backout.rb
@@ -1,4 +1,8 @@
 class Backout < ActiveRecord::Base
   validates :item1, presence: true
   validates :amount1, presence: true
+  validates :item2, presence: true
+  validates :amount2, presence: true
+  validates :item3, presence: true
+  validates :amount3, presence: true
 end
