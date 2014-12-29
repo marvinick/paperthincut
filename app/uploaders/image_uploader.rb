eceef146fb0feb872eea2d07bf65f71740f37c90
marvinick <marviluw@gmail.com>
2014-12-29 15:36:36 -0600

    menu_item

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  include CarrierWave::MimeTypes
-  process :set_content_type
 
   # Choose what kind of storage to use for this uploader:
   storage :file
