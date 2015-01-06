1e5d96918a8a3f2da798115ab3b31033d6b008c4
marvinick <marviluw@gmail.com>
2015-01-06 07:19:41 -0600

    navbar

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
     provider: "AWS",
     aws_access_key_id: "AKIAJ2PO7UNRHWVMN6HA",
     aws_secret_access_key: "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"
+    "
   }
   config.fog_directory = "paprthin"
 end
