0e8a392a37410703b3fe1bedf87687977bcccde8
marvinick <marviluw@gmail.com>
2015-01-17 13:36:59 -0600

    appointment_controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,9 +1,33 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :destroy, :edit, :update]
+
   def index
     @appointments = Appointment.all
   end
 
   def new
     @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+
+    if @appointment.save
+      redirect_to appointments_path
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  private
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def appointment_params
+    param.require(:appointment).permit(:chef, :costumer)
   end
 end
