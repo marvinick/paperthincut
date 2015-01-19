b4017d1fe52fe93cbe24d3430a17cec355b3078a
marvinick <marviluw@gmail.com>
2015-01-18 19:24:45 -0600

    logs controller completed

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,48 @@
+class LogsController < ApplicationController
+  before_action :set_log, only: (:show, :edit, :update, :destroy)
+
+  def index
+    @logs = Log.all
+  end
+
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+
+    if @log.save
+      redirect_to logs_path
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @log.update(log_params)
+      redirect_to logs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @log.destroy
+    redirect_to logs_path
+  end
+
+  private
+
+  def set_log
+    @log = Log.find(params[:id])
+  end
+
+  def log_params
+    param.require(:log).permit(:description, :event)
+  end
+end
