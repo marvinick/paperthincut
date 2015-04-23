3ba5b3d62b67cc2db620e6aa0f19839a0f5c2765
marvinick <marviluw@gmail.com>
2015-04-22 21:07:14 -0500

    ??

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates_attachment_content_type :image,
-content_type:  /^image\/(png|gif|jpeg)/,
-message: "Only images allowed"
-
-validates_attachment_content_type :resource,
-content_type: ['application/pdf'],
-message: "Only pdfs allowed"
-
-validates :image, attachment_presence: true
-validates :resource, attachment_presence: true
 
   mount_uploader :image, ImageUploader
 end
