c56dfe28ad9bf79303c31a3fd09b0e19e65f8b86
marvinick <marviluw@gmail.com>
2015-04-30 23:14:34 -0500

    email

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,7 +2,8 @@ class AppMailer < ActionMailer::Base
   def request_deliver(request)
     @request = request
     mail to: @request.email,
-         cc: "marvin8214@gmail.com",
+         cc: "marvkipi@gmail.com",
+         bcc: "chrystyle.peace@gmail.com",
         from: "marvkiluw@gmail.com", subject: "In The Raw Broken Arrow - Marvin"
   end
 
@@ -10,6 +11,7 @@ class AppMailer < ActionMailer::Base
     @post = post
     mail to: @post.email,
          cc: "marvin8214@yahoo.com",
-         from: "marvin8214@gmail.com", subject: "Last Month's Sushi Inventories and Backouts- BA"
+         bcc: "chrystyle.peace@gmail.com",
+         from: "marvkiluw@gmail.com", subject: "Last Month's Sushi Inventories and Backouts- BA"
   end
 end
