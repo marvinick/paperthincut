cee05fe11dd16e5510fa6650b2b23f5b3dc25ae3
marvinick <marviluw@gmail.com>
2015-05-03 18:22:21 -0500

    total_price

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -11,6 +11,7 @@ class Basket < ActiveRecord::Base
     current_item
   end
 
-  def total_cost
+  def total_price
+    items.to_a.sum { |item| item.total_price }
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :good
   belongs_to :basket
   belongs_to :request
+
+  def total_price
+    good.price * amount
+  end
 end
