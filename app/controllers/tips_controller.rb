2c737265e8e08e835cc800cf0530fe02a1b36dcf
marvinick <marviluw@gmail.com>
2015-05-23 11:13:41 -0500

    tip view

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -4,5 +4,22 @@ class TipsController < ApplicationController
   end
 
   def new
+    @tip = Tip.new
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    @tip.save
+    redirect_to tips_path
+  end
+
+  def show
+    @tip = Tip.find(params[:id])
+  end
+
+  private
+
+  def tip_params
+    params.require(:tip).permit(:month, :week)
   end
 end
