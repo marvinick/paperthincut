8aac41eef3345b7c9a043350ce6f919446a070c4
marvinick <marviluw@gmail.com>
2015-05-25 06:55:29 -0500

    finished writing spec for Rate

--- spec/features/user_rates_roll_spec.rb
+++ spec/features/user_rates_roll_spec.rb
@@ -3,5 +3,11 @@ require 'spec_helper'
 feature "User rates roll" do
   scenario "no auth" do
     visit root_path
+    click_on "RateIt"
+    fill_in "Flavor", with: 2
+    fill_in "Texture", with: 4
+    fill_in "Health", with: 3
+    fill_in "Visual", with: 5
+    click_on "Submit"
   end
 end
